73b9836efa113c7f3385ba0825a20a95d8c481a0
shreyaballijepalli <cs15btech11009@iith.ac.in>
2017-07-23 11:59:06 +0530

    Update update.js

--- update.js
+++ update.js
@@ -19,7 +19,7 @@ var execPhp = require('exec-php');
 execPhp('/var/www/html/sql.php', function(error, php, outprint){
     
     shell.exec('sudo \cp  /var/www/html/chat.json ./');
-    shell.exec('curl -H "Content-Type: application/json" -X POST -u "59e64644-72af-4bc3-a4e9-af6b9ff01ff2":"lHXAV0sMiGea" --data-binary @chat.json "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/d657b9d9-2029-4c19-a52e-a95264031250?version=2017-05-26"');
+    shell.exec('curl -H "Content-Type: application/json" -X POST -u "":"" --data-binary @chat.json "https://gateway.watsonplatform.net/conversation/api/v1/workspaces/?version=2017-05-26"');
 });
 
 
